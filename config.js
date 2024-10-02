6d642bd55025e5f0c8f3d9562cc44ae0e6f1431f
Vesa Meskanen <vesa.meskanen@cgi.com>
2024-10-02 10:45:18 +0300

    Remove empty HSLlautta package, it breaks dataloading

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ const routers = {
     id: 'hsl',
     src: [
       mapSrc('HSL', 'https://infopalvelut.storage.hsldev.com/gtfs/hsl.zip', false, undefined, { 'trips.txt': 'trips2.txt' }),
-      mapSrc('HSLlautta', 'https://koontikartta.navici.com/tiedostot/gtfs_lautat_digitransit.zip')
+      // mapSrc('HSLlautta', 'https://koontikartta.navici.com/tiedostot/gtfs_lautat_digitransit.zip')
       // src('Sipoo', 'https://koontikartta.navici.com/tiedostot/rae/sipoon_kunta_sibbo_kommun.zip')
     ],
     osm: ['hsl'],
