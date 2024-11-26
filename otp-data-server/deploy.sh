1839cd95b46b288914b585ca1beb84a5ef012ea9
Eetu Tuomala <4726921+laardee@users.noreply.github.com>
2024-11-26 14:50:27 +0200

    Flip defined

--- otp-data-server/deploy.sh
+++ otp-data-server/deploy.sh
@@ -14,9 +14,9 @@ CONTAINER=opentripplanner-data-server
 DOCKER_IMAGE=$ORG/$CONTAINER
 
 if [[ -z "${DOCKER_USER}" ]]; then
-  docker login -u $DOCKER_USER -p $DOCKER_AUTH
-else
   echo "*** DOCKER_USER is not defined. Unable to log in to the registry."
+else
+  docker login -u $DOCKER_USER -p $DOCKER_AUTH
 fi
 
 DOCKER_DATE_IMAGE=$DOCKER_IMAGE:$DOCKER_TAG-$ROUTER_NAME-$DATE
@@ -27,10 +27,10 @@ docker build -t $DOCKER_DATE_IMAGE .
 docker tag $DOCKER_DATE_IMAGE $DOCKER_IMAGE_TAGGED
 
 if [[ -z "${DOCKER_USER}" ]]; then
+  echo "*** Not signed into the registry. Image not pushed."
+else
   echo "*** Pushing $DOCKER_DATE_IMAGE"
   docker push $DOCKER_DATE_IMAGE
   echo "*** Pushing $DOCKER_IMAGE_TAGGED"
   docker push $DOCKER_IMAGE_TAGGED
-else
-  echo "*** Not signed into the registry. Image not pushed."
 fi
